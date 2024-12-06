b2a2c2679f24fdf8a8011e210754fe05dcacfcfd
Ama-Nkansah <amankansahstate@gmail.com>
2024-12-06 17:18:42 +0000

    adding javascript to my sidebar

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const hamburgerIcon = document.getElementById("hamburger");
+const closeButton = document.getElementById("close-button");
+const sideBar = document.getElementById("sidebar");
 
+hamburgerIcon.addEventListener("click", ()=>
+  {
+  sideBar.classList.toggle("translate-x-0");
+
+  if
+  (sideBar.classList.contains("translate-x-0")){
+    hamburgerIcon.classList.remove("fa-bars");
+    hamburgerIcon.classList.add("fa-square-xmark");
+  }
+  else{
+    hamburgerIcon.classList.add("fa-bars");
+    hamburgerIcon.classList.remove("fa-square-xmark");
+  }
+
+});
