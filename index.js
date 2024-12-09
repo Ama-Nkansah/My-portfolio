ffb4db777518324cdcbc2913e947907a4d097072
Ama-Nkansah <amankansahstate@gmail.com>
2024-12-09 18:06:44 +0000

    styling my navigation bar and adding animations

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const hamburgerIcon = document.getElementById("hamburger");
-const closeButton = document.getElementById("close-button");
 const sideBar = document.getElementById("sidebar");
+const hamburgerButton = document.getElementById("hamburger-button");
+const closeButton = document.getElementById("close-button");
 
-hamburgerIcon.addEventListener("click", ()=>
-  {
-  sideBar.classList.toggle("translate-x-0");
+hamburgerButton.addEventListener("click", () => {
+  sideBar.classList.remove("hidden");
+  sideBar.classList.add("flex");
+  hamburgerButton.classList.remove("inline-flex");
+  hamburgerButton.classList.add("hidden");
+  closeButton.classList.remove("hidden");
+  closeButton.classList.add("flex");
+});
 
-  if
-  (sideBar.classList.contains("translate-x-0")){
-    hamburgerIcon.classList.remove("fa-bars");
-    hamburgerIcon.classList.add("fa-square-xmark");
-  }
-  else{
-    hamburgerIcon.classList.add("fa-bars");
-    hamburgerIcon.classList.remove("fa-square-xmark");
-  }
-
+closeButton.addEventListener("click", () => {
+sideBar.classList.add("hidden");
+hamburgerButton.classList.remove("hidden");
+hamburgerButton.classList.add("inline-flex");
 });
