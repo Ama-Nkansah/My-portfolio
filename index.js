dc4d17bef7f99f153d93e7f7d9b9dad291d7446f
Ama-Nkansah <amankansahstate@gmail.com>
2024-12-16 11:49:47 +0000

    done styling my header

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const sideBar = document.getElementById("sidebar");
 const hamburgerButton = document.getElementById("hamburger-button");
 const closeButton = document.getElementById("close-button");
+const navBar = document.getElementById("nav");
+const logo1 = document.getElementById("logo");
+const overlay = document.getElementById("overlay");
+
 
 hamburgerButton.addEventListener("click", () => {
   sideBar.classList.remove("hidden");
   sideBar.classList.add("flex");
+  navBar.classList.add("pt-0","pr-0");
+  logo1.classList.add("p-4");
   hamburgerButton.classList.remove("inline-flex");
   hamburgerButton.classList.add("hidden");
   closeButton.classList.remove("hidden");
   closeButton.classList.add("flex");
+  overlay.classList.remove("hidden");
+  overlay.classList.add("flex");
 });
 
 closeButton.addEventListener("click", () => {
