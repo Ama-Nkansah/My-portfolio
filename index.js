ec67ab6537af71b99e1df855570ee716179966d7
Ama Nkansah <amankansahstate@gmail.com>
2025-01-16 14:11:01 +0000

    added submit functionality and alert to it

--- index.js
+++ index.js
@@ -23,26 +23,29 @@ hamburgerButton.classList.remove("hidden");
 hamburgerButton.classList.add("inline-flex","pr-10");
 });
 
+//when the whole page is loaded this should happen
 $(document).ready(function(){
+  //go to the contact form and perform the function when submit is trigerred
+
   $('#contact-form').on('submit',function(e){
+    e.preventDefault(); //prevent page reload when the submit is trigerred and i can do what i want next 
+    console.log('Submit triggered');
+    
+    const name = $('#name').val(); // Get the name value
+    const email = $('#email').val(); // Get the email value
+    const message = $('#message').val(); // Get the message value
+
+    if (name.trim() === '' || email.trim() === '' || message.trim() === '') {
+        alert('Please fill in all fields!');
+    }  else {
+      alert(`Name: ${name}, Email: ${email}, Message: ${message}`);
+      $('#name').val(''); // Clear the name field
+      $('#email').val(''); // Clear the email field
+      $('#message').val(''); // Clear the message field
+  }
 
   });
-
-}
 });
 
-/*
-$(document).ready(function() {
-    $('#contactForm').on('submit', function(e) {
-        e.preventDefault(); // Prevent page reload
-        const message = $('#message').val(); // Get the message value
-        if (message.trim() === '') {
-            alert('Please type a message!');
-        } else {
-            alert(`Message received: ${message}`);
-            $('#message').val(''); // Clear the message area
-        }
-    });
-});
 
-*/ 
+
