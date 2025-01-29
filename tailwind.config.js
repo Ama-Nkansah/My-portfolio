d88e7abdd96719d02f293c78ca64db5609b104a5
Ama Nkansah <amankansahstate@gmail.com>
2025-01-29 20:05:26 +0000

    navigating through tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      screens: {
+        biggersm: '510px', // Custom breakpoint
+      },
+      colors: {
+        clifford: '#da373d', // Custom color
+      },
+    },
+  },
+  plugins: [],
+};
